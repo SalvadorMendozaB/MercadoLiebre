a60ae62c34597989bbfd8b8ed5b096b250badd6e
SalvadorMendozaB <SalvadorMen3@gmail.com>
2021-10-07 20:09:21 -0500

    realizacion de los cambios necesarios para heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
   console.log("Todo se conecto bien");
 });
 
