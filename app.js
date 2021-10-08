84a5fb75c1ddfaf6af8e5b3080b720d49d1d082f
SalvadorMendozaB <SalvadorMen3@gmail.com>
2021-10-07 19:26:25 -0500

    Iniciando el repositorio

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3000, () => {
+  console.log("Todo se conecto bien");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
+
+app.get("/ingreso", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
